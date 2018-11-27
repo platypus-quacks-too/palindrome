bd5ba5bb1beba31ac648af3278fb88586834666f
platypus-quacks-too <nanyscia@yahoo.it>
2018-11-27 14:57:55 +0100

    Finish working and refactored palidrome method.

--- index.js
+++ index.js
@@ -15,17 +15,12 @@ function Phrase(content) {
   }
 
   // Returns the letters in the content.
-  // For example:
-  //    new Phrase("Hello, world!").letters() === "Helloworld"
-  this.letters = function letters() {
-    let theLetters = [];
-    for (let i = 0; i < this.content.length; i++) {
-      if (this.content.charAt(i).match(/[a-zA-Z]/)) {
-        theLetters.push(this.content.charAt(i));
-      }
-    }
-    return theLetters.join("");
-  }
+   // For example:
+   //   new Phrase("Hello, world!").letters() === "Helloworld"
+   this.letters = function letters() {
+     return (this.content.match(/[a-zA-Z]/gi) || [] ).join("");
+     //return Array.from(this.content).filter(c => c.match(/[a-z]/i)).join("");
+   }
 
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
